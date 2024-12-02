fbd694e504e76e6f832c053fe565c2c5d42d5ec9
Darius <dkavaliukas@gmail.com>
2024-12-02 15:52:27 +0200

    3nd commit

--- autoservice/views.py
+++ autoservice/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.contrib.admindocs.views import BookmarkletsView
+
 from .models import Service, CarModel, Car, Order, OrderLine
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 def index(request):
     count_services = Service.objects.all().count()
@@ -13,3 +16,22 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+def cars(request):
+    cars = Car.objects.all()
+    context = {
+        'cars': cars
+    }
+    return render(request, 'cars.html', context=context)
+
+def car(request, national_id):
+    single_car = get_object_or_404(Car, pk=national_id)
+    return render(request, 'car.html', {'car':single_car})
+
+class OrdersView(generic.ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+class OrderDetailView(generic.DetailView):
+    model = Order
+    template_name = 'order_detail.html'
